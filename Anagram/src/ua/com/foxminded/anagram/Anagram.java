8793d390f53eaa9a34eaa4e481e62568bac1da14
Ivan Ivanou <ii99xt1@gmail.com>
2021-03-20 01:05:26 +0300

    program ready

--- Anagram/src/ua/com/foxminded/anagram/Anagram.java
+++ Anagram/src/ua/com/foxminded/anagram/Anagram.java
@@ -1,10 +1,40 @@
 package ua.com.foxminded.anagram;
 
+import java.util.Scanner;
+
 public class Anagram {
 
 	public static void main(String[] args) {
-		
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input text for anagram: ");
+		String inputText = in.nextLine();
+		in.close();
+		System.out.println(reverseWords(inputText));
 	}
 
+	public static String reverseWords(String input) {
+		int i = 0;
+		String inputWords[] = input.split(" ");
+		String outputWords[] = new String[inputWords.length];
+
+		for (String inputWord : inputWords) {
+			StringBuilder onlyLetterWord = new StringBuilder();
+			for (int j = 0; j < inputWord.length(); j++) {
+				if (Character.isLetter(inputWord.charAt(j))) {
+					onlyLetterWord.append(inputWord.charAt(j));
+				}
+			}
+			onlyLetterWord.reverse();
+			for (int j = 0; j < inputWord.length(); j++) {
+				if (!Character.isLetter(inputWord.charAt(j))) {
+					onlyLetterWord.insert(j, inputWord.charAt(j));
+				}
+			}
+
+			String reverse = onlyLetterWord.toString();
+			outputWords[i] = reverse;
+			i++;
+		}
+		return String.join(" ", outputWords);
+	}
 }
