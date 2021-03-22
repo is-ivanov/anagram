7c27629e3689c0d78ca16150df5643ce62c5c643
Ivan Ivanou <ii99xt1@gmail.com>
2021-03-22 22:38:46 +0300

    Fixed code after comments

--- Anagram/test/ua/com/foxminded/anagram/AnagramTest.java
+++ Anagram/test/ua/com/foxminded/anagram/AnagramTest.java
@@ -5,14 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class AnagramTest {
-
+	final String ONLY_LETTERS = "abcd efgh";
+	final String ONLY_LETTERS_REVERSED = "dcba hgfe";
+	final String LETTERS_AND_OTHER = "a1bcd efg!h";
+	final String LETTERS_AND_OTHER_REVERSED = "d1cba hgf!e";
+	
+	
 	@Test
 	void testOnlyLetters() {
-		assertEquals("dcba hgfe", Anagram.reverseWords("abcd efgh"));
+		assertEquals(ONLY_LETTERS_REVERSED, Anagram.reverseWords(ONLY_LETTERS));
 	}
+	
 	@Test
-	void testLettersAndNonLetters() {
-		assertEquals("d1cba hgf!e", Anagram.reverseWords("a1bcd efg!h"));
+	void testLettersAndOther() {
+		assertEquals(LETTERS_AND_OTHER_REVERSED, Anagram.reverseWords(LETTERS_AND_OTHER));
 	}
 
 }
