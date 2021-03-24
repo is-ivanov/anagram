5e3344f645efd416f512f0b18d079c4a7681cff7
Ivan Ivanou <ii99xt1@gmail.com>
2021-03-24 22:18:20 +0300

    Moved the creation object into a separate method

--- test/ua/com/foxminded/anagram/AnagramTest.java
+++ test/ua/com/foxminded/anagram/AnagramTest.java
@@ -10,16 +10,18 @@ class AnagramTest {
 	final String LETTERS_AND_OTHER = "a1bcd efg!h";
 	final String LETTERS_AND_OTHER_REVERSED = "d1cba hgf!e";
 
-	
+	WordReverser beforeAll() {
+		WordReverser wordsTest = new WordReverser();
+		return wordsTest;
+	}
+
 	@Test
 	void testOnlyLetters() {
-		WordReverser wordsTest = new WordReverser();
-		assertEquals(ONLY_LETTERS_REVERSED, wordsTest.reverseWords(ONLY_LETTERS));
+		assertEquals(ONLY_LETTERS_REVERSED, beforeAll().reverseWords(ONLY_LETTERS));
 	}
-	
+
 	@Test
 	void testLettersAndOther() {
-		WordReverser wordsTest = new WordReverser();
-		assertEquals(LETTERS_AND_OTHER_REVERSED, wordsTest.reverseWords(LETTERS_AND_OTHER));
+		assertEquals(LETTERS_AND_OTHER_REVERSED, beforeAll().reverseWords(LETTERS_AND_OTHER));
 	}
 }
