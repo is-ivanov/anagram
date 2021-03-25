7852d5c63b5dc23267ca986fefc4413b26b48424
Ivan Ivanou <ii99xt1@gmail.com>
2021-03-25 17:34:10 +0300

    change AnagramTest

--- test/ua/com/foxminded/anagram/AnagramTest.java
+++ test/ua/com/foxminded/anagram/AnagramTest.java
@@ -10,20 +10,21 @@ class AnagramTest {
 	final String ONLY_LETTERS_REVERSED = "dcba hgfe";
 	final String LETTERS_AND_OTHER = "a1bcd efg!h";
 	final String LETTERS_AND_OTHER_REVERSED = "d1cba hgf!e";
+	WordReverser wordsTest;
+	
 	
 	@BeforeAll
-	public static WordReverser createObject() {
-		WordReverser wordsTest = new WordReverser();
-		return wordsTest;
+	public void createObject() {
+		wordsTest = new WordReverser();
 	}
 
 	@Test
 	void testOnlyLetters() {
-		assertEquals(ONLY_LETTERS_REVERSED, createObject().reverseWords(ONLY_LETTERS));
+		assertEquals(ONLY_LETTERS_REVERSED, wordsTest.reverseWords(ONLY_LETTERS));
 	}
 
 	@Test
 	void testLettersAndOther() {
-		assertEquals(LETTERS_AND_OTHER_REVERSED, createObject().reverseWords(LETTERS_AND_OTHER));
+		assertEquals(LETTERS_AND_OTHER_REVERSED, wordsTest.reverseWords(LETTERS_AND_OTHER));
 	}
 }
